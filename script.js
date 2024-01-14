64690dce415c5078fd72827fb3f0f2eb33b898c7
dogwater53 <150091545+dogwater53@users.noreply.github.com>
2024-01-14 16:00:22 -0700

    added key & darker mode

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 let textinp;
 let emojiinp;
-window.onload = () => {
-	textinp = document.getElementById("text");
-	emojiinp = document.getElementById("emoji");
-	
-}
+let keyinp;
 
 let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
 let emojis = ['🥪','🍆','🔨','🔧','🛠️','⚒️','🤼‍♂️','💀','☠️','👽','👻','🧌','🗣️','💯','⁉️','🔥','❓','🪛','🧰','🪚','🗜️','⚙️','⛏️','🔩','⚗️','🐪','🪇','🧯','💳','💡','❤️‍🔥','🆗','🥦','🍑','🙀','😻','🍔','🐷','🐶','🙈','🐮','🐸','🔫','🐦','🐈','😩','🫵','👍','👎','🔞','❤️‍🩹','🥓','💔','🪤','🥵','🤯','🥶','🧀','🎮','🤪','💸','🧽','🪑','🪣','‼️'];
 
+window.onload = () => {
+	textinp = document.getElementById("text");
+	emojiinp = document.getElementById("emoji");
+	keyinp = document.getElementById("key");
+}
+
+function getKey() {
+	return parseInt(key.value) & 63;
+}
+
 function toEmoji() {
+	let key = getKey();
 	let inp = textinp.value;
 	let out = "";
 	for (char of btoa(inp)) {
 		let inx = chars.indexOf(char);
+		inx = (inx & 64) ? inx : inx ^ key;
 		out = out + emojis[inx];
-		if (emojis[inx] === undefined) {
-			console.log(inx);
-		}
 	}
 	emojiinp.value = out;
 }
 
 function toText() {
+	let key = getKey();
 	let inp = emojiinp.value;
 	let out = "";
 	let inter = "";
@@ -32,10 +38,9 @@ function toText() {
 			continue;
 		}
 		let inx = emojis.indexOf(inter);
-		console.log(inx);
+		inx = (inx & 64) ? inx : inx ^ key;
 		inter = "";
 		out = out + chars[inx];
 	}
 	textinp.value = atob(out);
-
 }
